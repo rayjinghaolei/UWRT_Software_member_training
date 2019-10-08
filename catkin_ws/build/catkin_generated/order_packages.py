ea54d6ae74ac9672ffc906c64f3ec3e5635c3d57
Ray Jinghao Lei <45613893+rayjinghaolei@users.noreply.github.com>
2019-10-07 20:42:22 -0400

    commiting member training code

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/raylei/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/raylei/catkin_ws/devel;/opt/ros/melodic".split(';') if "/home/raylei/catkin_ws/devel;/opt/ros/melodic" != "" else []
