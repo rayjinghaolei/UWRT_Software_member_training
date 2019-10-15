9e9a579146ef5f1b89872f12ab28f4f49891be40
Ray Jinghao Lei <45613893+rayjinghaolei@users.noreply.github.com>
2019-10-15 13:45:54 -0400

    software_member_training update until ros msg and srv, but unable to roscd my workspace 'software_member_training'

---
+++ catkin_ws/src/software_member_training/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/raylei/Github/UWRT_Software_member_training/catkin_ws/src/software_member_training/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/opt/ros/melodic".split(';') if "/opt/ros/melodic" != "" else []
---
+++ catkin_ws/src/software_member_training/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/raylei/Github/UWRT_Software_member_training/catkin_ws/src/software_member_training/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
